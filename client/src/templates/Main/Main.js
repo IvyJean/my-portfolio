0e2e5f932af8feae67005db07e9d7e4b9fef9ad7
ivy.cocharo <ivy.cocharo@whitecloak.com>
2021-06-10 17:42:56 +0800

    Added initial layout

---
+++ client/src/templates/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../../layouts/Layout/Layout';
+
+const Main = () => {
+  return (
+    <Layout>
+      Home
+    </Layout>
+  )
+}
+
+
+export default Main
